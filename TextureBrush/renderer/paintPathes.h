edb1975c9158545fe4d12186c00cf1b81b38bb0d
wadmes <werry715@gmail.com>
2016-07-24 16:41:37 +0800

    Update paintPathes.h
    fix some bugs

--- TextureBrush/renderer/paintPathes.h
+++ TextureBrush/renderer/paintPathes.h
@@ -19,8 +19,8 @@ public:
 	void extractTriangleIndexTexture(GLuint texId);
 	void compute3dPath();
 
-	float DistanceFromPointToPath(vec3 point, vector<ivec2> m_pathPointVec);
-	void AddPointFunc(int TriNum, ivec3* pTriIndices, vector<ivec2> m_pathPointVec, vector<int> &newPathTriangleIdxVec, vector<int> &CN);
+	float DistanceFromPointToPath(vec3 point, vector<ivec2> m_pathPointVec,int winHeight);
+	void AddPointFunc(int TriNum, ivec3* pTriIndices, vector<ivec2> m_pathPointVec, vector<int> &newPathTriangleIdxVec, vector<int> &CN, int winHeight);
 
 	const vector<ivec2>& getPathPointVec(){ return m_pathPointVec; }
 	const vector<vector<ivec2> >& getPathVec(){ return m_pathVec; }
